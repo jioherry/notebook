c8cff458cc0ffa55a8256ca365f9c2a061a86ac2
jioherry <jioherry@gmail.com>
2017-04-01 16:16:34 +0800

    提供後台管理介面，後台可以 CRUD 文章的分類，不能刪除已經有被使用的分類（未完成）

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,48 @@
+class Admin::CategoriesController < ApplicationController
+
+	before_action :authenticate
+    layout "admin"
+
+	def index
+		@categories = Category.all
+	end
+
+	def new
+		@category = Category.new
+	end
+
+	def creat
+		@category = Category.new(category_params)
+		@category.save
+
+		redirect_to admin_categories_url
+	end
+
+	def edit
+		@category = Category.find(params[:id])
+	end
+
+	def update
+		@category = Category.find(params[:id])
+		@category.update(category_params)
+
+		redirect_to admin_categories_url
+	end
+
+	def destroy
+		# 如何找到未使用過的 category
+	end
+
+	private
+
+		def authenticate
+	    authenticate_or_request_with_http_basic do |user_name, password|
+	      user_name == "12345" && password == "12345"
+	    end
+	  end
+
+		def category_params
+			params.require(:category).permit(:name, :topic_id)
+		end
+
+end
